bcf1348886938b39b3fe644154198d790a70bb79
ChenZhenY <1207261079@qq.com>
2022-03-18 17:24:14 +0000

    sensor_model not verified

--- src/localization/sensor_model.py
+++ src/localization/sensor_model.py
@@ -1,3 +1,4 @@
+from locale import normalize
 import numpy as np
 from scan_simulator_2d import PyScanSimulator2D
 
@@ -19,11 +20,16 @@ class SensorModel:
         ####################################
         # TODO
         # Adjust these parameters
-        self.alpha_hit = 0
-        self.alpha_short = 0
-        self.alpha_max = 0
-        self.alpha_rand = 0
-        self.sigma_hit = 0
+        self.alpha_hit = 0.74
+        self.alpha_short = 0.07
+        self.alpha_max = 0.07
+        self.alpha_rand = 0.12
+        self.sigma_hit = 8.0
+
+        zmin = 0
+        zmax = 10
+        sigma_hit = 8
+        eps = 1 # for Pmax
 
         # Your sensor table will be a `table_width` x `table_width` np array:
         self.table_width = 201
@@ -69,6 +75,51 @@ class SensorModel:
         returns:
             No return type. Directly modify `self.sensor_model_table`.
         """
+
+        zmin = self.zmin
+        zmax = self.zmax
+        sigma_hit = self.sigma_hit
+        eps = self.eps # for Pmam
+
+        def Phit(zki, eta, sigma=sigma_hit, d=7, zmax=zmax):
+            if zki>=0 and zki<=zmax:
+                return eta*np.sqrt(2*np.pi*sigma**2)*np.exp(-(zki-d)**2/(2*sigma**2))
+            return 0
+
+        def Pshort(zki, d=7):
+            if zki>=0 and zki<d and d!=0:
+                return 2/d*(1-zki/d)
+            return 0
+
+        def Pmax(zki, zmax=zmax, eps=eps):
+            if zki==zmax:
+                return 1/eps
+            return 0
+
+        def Prand(zki, zmax=zmax):
+            if zki>=0 and zki<=zmax:
+                return 1/zmax
+            return 0
+
+        def Pall(zki, din, eta, ahit = self.alpha_hit, ashort = self.alpha_short, amax = self.alpha_max, arand = self.alpha_rand):
+            return ahit*Phit(zki, d=din) + ashort*Pshort(zki, d=din) + amax*Pmax(zki)+arand*Prand(zki)
+        
+        # column for each actual distance, row for each measured distance
+        # val_list map the scale of dmin dmax to the actual value in the LUT, for d and zk, the list is the same (same range and delta)
+        val_list = range(zmin, zmax, self.table_width)
+        for d_col in range(self.table_width):
+
+            eta_sum = 0              # normalization factor for Phit 
+            for zk_row in range(self.table_width):
+                d_actual = val_list[d_col]
+                zk_actual= val_list[zk_row]
+                eta_sum += Phit(zk_actual, d=d_actual)
+
+            for zk_row in range(self.table_width):
+                self.sensor_model_table[zk_row, d_col] = Pall(zk_actual, d_actual, 1/eta_sum)
+
+            # normalize the whole column
+            self.sensor_model_table[:,d_col] /= sum(self.sensor_model_table[:,d_col]) 
         raise NotImplementedError
 
     def evaluate(self, particles, observation):
@@ -96,14 +147,26 @@ class SensorModel:
             return
 
         ####################################
-        # TODO
+        # TODO:
         # Evaluate the sensor model here!
         #
         # You will probably want to use this function
         # to perform ray tracing from all the particles.
-        # This produces a matrix of size N x num_beams_per_particle 
-
+        # This produces a matrix of size N x num_beams_per_particle
+        val_list = range(self.zmin, self.zmax, self.table_width)
         scans = self.scan_sim.scan(particles)
+        probability = np.zeros(particles.shape[0])
+        for i in range(particles.shape[0]):
+
+            zki = particles[i, :2].dot(particles[i, :2])
+            d = observation[:2].dot(observation[:2])
+
+            differencez = np.abs(zki-val_list)
+            indexz = differencez.argmin()
+            differenced = np.abs(zki-val_list)
+            indexd = differenced.argmin()
+
+            probability[i] = self.sensor_model_table[indexz, indexd]
 
         ####################################
 
