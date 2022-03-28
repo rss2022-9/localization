ad4bd12fe41c09d87e02a4440ab4290eafcb647b
Kwadwo A. Yeboah-Asare Jnr <35611062+Prekaya@users.noreply.github.com>
2022-03-28 15:57:41 -0400

    Update particle_filter.py

--- src/localization/particle_filter.py
+++ src/localization/particle_filter.py
@@ -30,39 +30,39 @@ class ParticleFilter:
         self.odom_pub  = rospy.Publisher("/pf/pose/odom", Odometry, queue_size = 1)
 
         # Initialize the models
-        self.motion_model = MotionModel()
-        self.sensor_model = SensorModel()
+        self.m_model = MotionModel()
+        self.s_model = SensorModel()
 
         # Initialize important variables
         self.lock = Lock()
-        self.num_particles = rospy.get_param("~num_particles")
-        self.size = (self.num_particles,3)
-        self.particles = np.zeros(self.size)
+        self.num_parts = rospy.get_param("~num_parts")
+        self.size = (self.num_parts,3)
+        self.parts = np.zeros(self.size)
         self.odom = Odometry()
         self.marker = Marker()
-        self.line_pub = rospy.Publisher(self.cloud_topic, Marker, queue_size=1)
+        self.line_pub = rospy.Publisher(cloud_topic, Marker, queue_size=1)
 
         # Implement the MCL algorithm
         # using the sensor model and the motion model
         #
         # Make sure you include some way to initialize
-        # your particles, ideally with some sort
+        # your parts, ideally with some sort
         # of interactive interface in rviz
         #
         # Publish a transformation frame between the map
         # and the particle_filter_frame.
 
-    def update_particles(self, new_particles):
+    def update_parts(self, new_parts):
         self.lock.acquire()
         try:
-            x = np.mean(new_particles[:,0])
-            y = np.mean(new_particles[:,1])
-            o = circmean(new_particles[:,2])
+            x = np.mean(new_parts[:,0])
+            y = np.mean(new_parts[:,1])
+            o = circmean(new_parts[:,2])
             odom_quat = tf.transformations.quaternion_from_euler(0, 0, o)
             self.odom.pose.pose = Pose(Point(x,y,0.0), Quaternion(*odom_quat))
             self.odom_pub.publish(self.odom)
-            VisualizationTools.plot_line(x, y, self.line_pub, frame="/map")
-            self.particles = new_particles
+            VisualizationTools.plot_line(new_parts[:,0], new_parts[:,1], self.line_pub, frame="/map")
+            self.parts = new_parts
         finally:
             self.lock.release()
 
@@ -70,25 +70,27 @@ class ParticleFilter:
         X = clickpose.pose.pose.position.x
         Y = clickpose.pose.pose.position.y
         R = 2
-        S = self.num_particles
+        S = self.num_parts
         phi = np.random.random(size=S)*2*np.pi
         rad = np.random.random(size=S)*R
         xlist = X + R*np.cos(phi)
         ylist = Y + R*np.sin(phi)
         olist = np.random.random(size=S)*2*np.pi
-        new_particles = np.column_stack((xlist,ylist,olist))
-        self.update_particles(new_particles)
+        new_parts = np.column_stack((xlist,ylist,olist))
+        self.update_parts(new_parts)
 
     def updposes(self,odom):
         inpodom = [odom.twist.twist.linear.x,odom.twist.twist.linear.y,odom.twist.twist.angular.z]
-        new_particles = self.motion_model.evaluate(self.particles,inpodom)
-        self.update_particles(new_particles)
-        
+        new_parts = self.m_model.evaluate(self.parts,inpodom)
+        self.update_parts(new_parts)
+
     def calcprobs(self,sensdata):
-        probs = self.sensor_model.evaluate(self.particles,np.copy(sensdata.ranges))
-        new_particles = self.particles[np.random.choice(self.particles.shape[0],size=self.num_particles,p=probs)]
-        self.update_particles(new_particles)
-    
+        pass
+        observation = np.copy(sensdata.ranges).flatten()
+        probs = self.s_model.evaluate(self.parts,observation)
+        new_parts = self.parts[np.random.choice(self.parts.shape[0],size=self.num_parts,p=probs)]
+        self.update_parts(new_parts)
+
 
 if __name__ == "__main__":
     rospy.init_node("particle_filter")
