587ed3c6d0b03ba516157c394535539e0b3105f6
Kwadwo A. Yeboah-Asare Jnr <35611062+Prekaya@users.noreply.github.com>
2022-03-30 17:20:51 -0400

    Add files via upload

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -3,15 +3,20 @@ import numpy as np
 class MotionModel:
 
     def __init__(self):
-        self.a1 = 0.05
-        self.a2 = 0.05
+        self.a1 = 0.001
+        self.a2 = 0.001
 
-    def evaluate(self, particles, odometry, noise=0):
+    def evaluate(self, particles, odometry, noise=1):
         S = particles.shape[0]
 
-        dx = odometry[0]
-        dy = odometry[1]
-        dtheta = odometry[2]
+        if noise:
+            dx = odometry[0] - np.random.normal(self.a1, size =(S,))
+            dy = odometry[1] - np.random.normal(self.a2, size =(S,))
+            dtheta = odometry[2] - np.random.normal(self.a1, size =(S,))
+        else:
+            dx = odometry[0]
+            dy = odometry[1]
+            dtheta = odometry[2]
 
         w_x = particles[:,0]
         w_y = particles[:,1]
@@ -25,14 +30,10 @@ class MotionModel:
         d_tran = otran
         d_rot2 = orot2
 
-        if noise:
-            x_new = w_x + d_tran*np.cos(d_rot1 + w_theta) - np.random.normal(np.sqrt(self.a1), size =(S,))
-            y_new = w_y + d_tran*np.sin(d_rot1 + w_theta) - np.random.normal(np.sqrt(self.a2), size =(S,))
-            o_new = w_theta + d_rot2 - np.random.normal(np.sqrt(self.a1), size =(S,))
-        else:
-            x_new = w_x + d_tran*np.cos(d_rot1 + w_theta)
-            y_new = w_y + d_tran*np.sin(d_rot1 + w_theta)
-            o_new = w_theta + d_rot2
+        
+        x_new = w_x + d_tran*np.cos(d_rot1 + w_theta)
+        y_new = w_y + d_tran*np.sin(d_rot1 + w_theta)
+        o_new = w_theta + d_rot2
         
         output = np.column_stack((x_new,y_new,o_new))
         
